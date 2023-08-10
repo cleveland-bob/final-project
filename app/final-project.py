12c90c27cc1972fa3ca0c81c91b0d81d210bba93
cleveland-bob <140764321+cleveland-bob@users.noreply.github.com>
2023-08-10 19:35:18 -0400

    added html & py code

--- app/final-project.py
+++ app/final-project.py
@@ -1,1 +1,39 @@
 #This is the app page
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from flask import Flask, render_template, request, redirect
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Google Sheets API setup
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("GolfScores").sheet1
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        hole = request.form["hole"]
+        score = int(request.form["score"])
+        putts = int(request.form["putts"])
+        fairway = request.form["fairway"]
+
+        # Log data in Google Sheet
+        sheet.append_row([hole, score, putts, fairway])
+
+        if int(hole) == 18:
+            return redirect("/summary")
+    
+    return render_template("index.html")
+
+@app.route("/summary")
+def summary():
+    data = sheet.get_all_values()
+    return render_template("summary.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
